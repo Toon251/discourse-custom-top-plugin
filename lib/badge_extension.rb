b7686dc066cac4416fb8deeedb99ed3130b3bf4f
Toon251 <toon251@gmail.com>
2025-05-15 07:43:27 +0000

    Support user badges

---
+++ lib/badge_extension.rb
@@ -0,0 +1,19 @@
+module BadgeExtension
+  def badges
+    user_badges.map do |user_badge|
+      {
+        id: user_badge.badge.id,
+        name: user_badge.badge.name,
+        description: user_badge.badge.description,
+        icon: user_badge.badge.icon
+      }
+    end
+  end
+end
+
+# โหลด Extension ใน Model User
+after_initialize do
+  User.class_eval do
+    include BadgeExtension
+  end
+end
